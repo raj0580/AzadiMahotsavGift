f1500ce64d1e2b75e0c2c54f78ccd6daa6d65414
raj0580 <duckdikcow@gmail.com>
2025-07-29 11:41:13 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqoN7rA8ql_iJfGpcZGhKuFi5tGSPbXAc",
+  authDomain: "story-website-afab7.firebaseapp.com",
+  databaseURL: "https://story-website-afab7-default-rtdb.firebaseio.com",
+  projectId: "story-website-afab7",
+  storageBucket: "story-website-afab7.firebasestorage.app",
+  messagingSenderId: "207838308502",
+  appId: "1:207838308502:web:fa12c413c45207f0bdbab7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
